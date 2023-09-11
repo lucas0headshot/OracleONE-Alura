6c84feddd81e77e22bc1c5ec6e349119eeb8c931
lucas0headshot <lucas0headshot@gmail.com>
2023-09-11 16:08:44 -0300

    Níveis de visibilidade entre pacotes e classes

---
+++ Java/OO - Orientação a Objetos/OO_Heritage/src/br/com/lucasbank/conta/especial/ContaEspecial.java
@@ -0,0 +1,15 @@
+package br.com.lucasbank.conta.especial;
+
+import br.com.lucasbank.conta.modelo.Conta;
+
+public class ContaEspecial extends Conta {
+	public ContaEspecial(int numero, int agencia) {
+		super(numero, agencia);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+}
