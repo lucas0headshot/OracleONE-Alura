b7937753c187d0cd3c48c32db5953bf2f0142941
lucas0headshot <lucas0headshot@gmail.com>
2023-09-10 23:55:51 -0300

    Lançando exceções c/ Java

---
+++ Java/Intermediário/Exceptions/src/executionStack/FluxoComTratamento.java
@@ -0,0 +1,30 @@
+package executionStack;
+
+public class FluxoComTratamento {
+	public static void main(String[] args) {
+        System.out.println("Ini do main");
+        try {
+        	metodo1();
+        } catch (Exception exception) {
+        	System.out.println("Exception: " + exception.getMessage());
+        	exception.printStackTrace();
+        }
+        System.out.println("Fim do main");
+	}
+	
+	
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        
+        throw new ArithmeticException("exceção lançada!");
+        
+//        System.out.println("Fim do metodo2");        
+    }
+}
