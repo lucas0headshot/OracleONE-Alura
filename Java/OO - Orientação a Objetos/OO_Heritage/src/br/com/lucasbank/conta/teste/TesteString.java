d623862875fa7952f0764efc39a794dbe58b6aa0
lucas0headshot <lucas0headshot@gmail.com>
2023-09-11 22:03:50 -0300

    Introdução ao java.lang

---
+++ Java/OO - Orientação a Objetos/OO_Heritage/src/br/com/lucasbank/conta/teste/TesteString.java
@@ -0,0 +1,13 @@
+package br.com.lucasbank.conta.teste;
+
+public class TesteString {
+	public static void main(String[] args) {
+		String nome = "Lucas";
+		
+		
+		
+		for (int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+		}
+	}
+}
